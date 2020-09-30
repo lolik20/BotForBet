e4ff3f6c2719190ecdb7ea9ff2f71fc39d6ae110
lolik20 <58035814+lolik20@users.noreply.github.com>
2020-09-30 14:38:07 +0300

    Add files via upload
    C#

---
+++ VKGroups — копия/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace VKGroups
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "СТАРТ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(282, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин VK";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(336, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль VK";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "На";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(92, 239);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(538, 239);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(221, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(541, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ссылка на группу VK";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(140, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(209, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "минуте создать пост";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(500, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(303, 26);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ссылка на группу Telegram";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(538, 309);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(221, 20);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Instagram mass like";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ VKGroups — копия/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+
+namespace VKGroups
+
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        IWebDriver Browser2;
+        IWebDriver Browser3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        public Bitmap TakeScreenshot(By by)
+        {
+            // 1. Make screenshot of all screen
+            var screenshotDriver = Browser as ITakesScreenshot;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            var bmpScreen = new Bitmap(new MemoryStream(screenshot.AsByteArray));
+
+            // 2. Get screenshot of specific element
+            IWebElement element = Browser.FindElement(by);
+            var cropArea = new Rectangle(element.Location, element.Size);
+            return bmpScreen.Clone(cropArea, bmpScreen.PixelFormat);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            var char_log = textBox1.Text;
+            var char_pass = textBox2.Text;
+            var time_input = textBox3.Text;
+            var link_group = textBox4.Text;
+            var link_telegram = textBox5.Text;
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser2 = new OpenQA.Selenium.Chrome.ChromeDriver();
+
+            Browser3.Navigate().GoToUrl("https://web.telegram.org");
+
+            Browser2.Navigate().GoToUrl("https://vk.com");
+            IWebElement login = Browser2.FindElement(By.Id("index_email"));
+            login.SendKeys(char_log);
+            IWebElement password = Browser2.FindElement(By.Id("index_pass"));
+            password.SendKeys(char_pass + OpenQA.Selenium.Keys.Enter);
+            System.Threading.Thread.Sleep(60000);
+
+
+
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("https://widgets.sir.sportradar.com/betradar/ru/head-to-head");
+            System.Threading.Thread.Sleep(3000);
+
+            System.Threading.Thread.Sleep(8000);
+            while (true)
+            {
+                Point:
+                try
+                {
+                    Clipboard.Clear();
+
+                    IWebElement headToHead = Browser.FindElement(By.XPath("/html/body/div/div/div/div[1]/div[3]/div[1]/div/a[4]"));
+                    headToHead.Click();
+                    IWebElement livematch = Browser.FindElement(By.XPath("/html/body/div/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/div/div[1]/div[3]"));
+                    livematch.Click();
+                    System.Threading.Thread.Sleep(5000);
+                    IWebElement time = Browser.FindElement(By.XPath("//*[text()=" + "'" + time_input + "’" + "'" + "]"));
+                    time.Click();
+                }
+                catch (NoSuchElementException)
+                {
+                    goto Point;
+                }
+
+
+                Browser3.Navigate().GoToUrl(link_telegram);
+                System.Threading.Thread.Sleep(5000);
+                string link = Browser.Url;
+
+
+                var prnt_scrn1 = TakeScreenshot(By.XPath("/html/body/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[4]/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[2]"));
+                Clipboard.SetImage(prnt_scrn1);
+                IWebElement telegramPost = Browser3.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[2]/div[5]"));
+                Browser2.Navigate().GoToUrl(link_group);
+                System.Threading.Thread.Sleep(3000);
+                IWebElement send_post1 = Browser2.FindElement(By.CssSelector("#post_field"));
+                send_post1.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                telegramPost.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                System.Threading.Thread.Sleep(2000);
+                IWebElement sendBtn = Browser3.FindElement(By.XPath("/html/body/div[7]/div[2]/div/div/div[2]/button[2]/span"));
+                sendBtn.Click();
+
+                System.Threading.Thread.Sleep(5000);
+                prnt_scrn1 = null;
+                Clipboard.Clear();
+
+                var prnt_scrn2 = TakeScreenshot(By.XPath("/html/body/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[4]/div/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div[1]"));
+
+                Clipboard.SetImage(prnt_scrn2);
+
+
+
+
+                send_post1.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                telegramPost.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                System.Threading.Thread.Sleep(2000);
+                sendBtn.Click();
+                prnt_scrn2 = null;
+                Clipboard.Clear();
+                System.Threading.Thread.Sleep(5000);
+                var prnt_scrn3 = TakeScreenshot(By.XPath("/html/body/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div[4]/div/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/div"));
+                Clipboard.SetImage(prnt_scrn3);
+
+
+
+
+                send_post1.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                telegramPost.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                System.Threading.Thread.Sleep(2000);
+                sendBtn.Click();
+                System.Threading.Thread.Sleep(5000);
+                IWebElement send_postbtn3 = Browser2.FindElement(By.CssSelector("#send_post"));
+                send_postbtn3.Click();
+                System.Threading.Thread.Sleep(5000);
+                prnt_scrn3 = null;
+                Clipboard.Clear();
+
+
+
+
+
+                GC.Collect();
+                System.Threading.Thread.Sleep(40000);
+                Browser.Navigate().GoToUrl("https://widgets.sir.sportradar.com/betradar/ru/head-to-head");
+
+
+                //   }
+                //     catch (NoSuchElementException)
+                //     {
+                //        goto Point;
+                //     }
+                //      catch ( OutOfMemoryException)
+                //       {
+
+                //       }
+                //     catch(NotFoundException)
+                //      {
+                //        goto Point;
+                //      }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
